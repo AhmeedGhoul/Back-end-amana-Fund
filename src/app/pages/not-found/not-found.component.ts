7b1d9c899027f25bb2a8c372e7db7aeff2c1a666
AhmeedGhoul <ahmed.ghoul@esprit.tn>
2025-05-09 13:15:56 +0100

    final

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
+import {MaterialModule} from "@app/material.module";
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
+  standalone: true,
+  imports: [
+    MaterialModule
+  ],
   styleUrls: ['./not-found.component.css']
 })
 export class NotFoundComponent {
